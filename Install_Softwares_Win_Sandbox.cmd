19e2965df44af41597f22004aaca536a856426aa
Tajamul Sheeraz <tajamulsheeraz@gmail.com>
2022-01-30 15:52:48 +0530

    Comments
    #Download ProcessHacker
    curl -L "https://github.com/processhacker/processhacker/releases/download/v2.39/processhacker-2.39-setup.exe" --output C:\Users\WDAGUtilityAccount\Desktop\processhacker.exe
    
    #Install ProcessHacker
    C:\Users\WDAGUtilityAccount\Desktop\processhacker.exe /verysilent /suppressmsgboxes

---
+++ Install_Softwares_Win_Sandbox.cmd
@@ -0,0 +1,3 @@
+curl -L "https://github.com/processhacker/processhacker/releases/download/v2.39/processhacker-2.39-setup.exe" --output C:\Users\WDAGUtilityAccount\Desktop\processhacker.exe
+
+C:\Users\WDAGUtilityAccount\Desktop\processhacker.exe /verysilent /suppressmsgboxes
